d48275011d14f30375de6928b0efc03881da4ee7
machukhinktato <tarabrinmv@gmail.com>
2021-01-12 18:34:30 +0300

    in progress

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello')
+import pytest
+
+def fizzBuzz(value):
+    return 1
+
+@pytest.mark.skip
+def test_functionCaller():
+    fizzBuzz(1)
+
+def test_functionAnswer():
+    ans = fizzBuzz(1)
+    assert ans == 1
