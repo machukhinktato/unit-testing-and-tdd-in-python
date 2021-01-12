d3771db560f8eae93bfdc060746a94e0e28662a6
machukhinktato <tarabrinmv@gmail.com>
2021-01-12 18:47:53 +0300

    in progress

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import pytest
 
-def fizzBuzz(value):
-    return 1
 
+# function which we build with ttd
+def fizzBuzz(value, expected):
+    if value == 3:
+        return expected
+    return str(value)
+
+
+# test that we could call fizzBuzz
 @pytest.mark.skip
 def test_functionCaller():
     fizzBuzz(1)
 
+
+#test that we receive value by result of calling
+@pytest.mark.skip
 def test_functionAnswer():
     ans = fizzBuzz(1)
     assert ans == 1
+
+
+#test that we receive any value by result of calling
+@pytest.mark.skip
+def test_functionAnswer_v2():
+    ans = fizzBuzz(2, '2')
+    assert ans == '2'
+
+
+#test that we receive any value by result of calling, including string name
+def test_functionAnswer_v3():
+    ans = fizzBuzz(3, 'fuzz')
+    assert ans == 'fuzz'
