f32c5d4423dcd43982ea9f942c54ef4670d4233c
machukhinktato <tarabrinmv@gmail.com>
2021-01-14 19:07:45 +0300

    in progress

---
+++ assert_statments.py
@@ -0,0 +1,21 @@
+import pytest
+from pytest import approx
+
+
+def test_IntAssert():
+    assert 1 == 1
+
+def test_StrAssert():
+    assert 'str' == 'str'
+
+def test_FloatAssert():
+    assert 1.0 == 1.0
+
+def test_ArrAssert():
+    assert [1,2,3] == [1,2,3]
+
+def test_DictAssert():
+    assert {'1':1} == {'1':1}
+
+def test_floAt():
+    assert (0.1 + 0.2) == approx(0.3)
