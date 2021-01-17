4fda4f262310f53ef52fd62fef5098c263f33195
machukhinktato <tarabrinmv@gmail.com>
2021-01-17 12:46:03 +0300

    in progress

--- section_5/setupnfirsttestclass.py
+++ section_5/setupnfirsttestclass.py
@@ -12,6 +12,9 @@ def checkout():
     checkout = Checkout()
     return checkout
 
+"""
+duplicating test_canCalculateTotal
+
 def test_CanAddItemPrice(checkout):
     # co = Checkout()
     checkout.addItemPrice('a', 1)
@@ -19,9 +22,17 @@ def test_CanAddItemPrice(checkout):
 def test_CanAddItem(checkout):
     # co = Checkout()
     checkout.addItem('a')
+"""
 
 def test_canCalculateTotal(checkout):
     checkout.addItemPrice('a', 1)
     checkout.addItem('a')
     assert checkout.calculateTotal() == 1
-    
+
+
+def test_getCorretTotalWithMultipleItems(checkout):
+    checkout.addItemPrice('a', 1)
+    checkout.addItemPrice('b', 2)
+    checkout.addItem('a')
+    checkout.addItem('b')
+    assert checkout.calculateTotal() == 3
