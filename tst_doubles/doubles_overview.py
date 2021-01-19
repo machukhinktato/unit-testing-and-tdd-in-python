6b1235d58e770a1e3e71a72f5d5860bfb0a8610f
machukhinktato <tarabrinmv@gmail.com>
2021-01-19 19:21:39 +0300

    in progress

--- tst_doubles/doubles_overview.py
+++ tst_doubles/doubles_overview.py
@@ -5,12 +5,19 @@ from unittest.mock import MagicMock
 
 # def test_canCallReadFromFile():
 #     readFromFile('blah')
-
-def test_returnsCorrectString(monkeypatch):
+@pytest.fixture()
+def mock_open(monkeypatch):
     mock_file = MagicMock()
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr('builtins.open', mock_open)
+    return mock_open
+
+def test_returnsCorrectString(mock_open, monkeypatch):
+    # mock_file = MagicMock()
+    # mock_file.readline = MagicMock(return_value="test line")
+    # mock_open = MagicMock(return_value=mock_file)
+    # monkeypatch.setattr('builtins.open', mock_open)
     mock_exists = MagicMock(return_value=True)
     monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile('blah')
@@ -18,11 +25,11 @@ def test_returnsCorrectString(monkeypatch):
     assert result == "test line"
 
 
-def test_throwsExceptionWithBadFile(monkeypatch):
-    mock_file = MagicMock()
-    mock_file.readline = MagicMock(return_value="test line")
-    mock_open = MagicMock(return_value=mock_file)
-    monkeypatch.setattr('builtins.open', mock_open)
+def test_throwsExceptionWithBadFile(mock_open, monkeypatch):
+    # mock_file = MagicMock()
+    # mock_file.readline = MagicMock(return_value="test line")
+    # mock_open = MagicMock(return_value=mock_file)
+    # monkeypatch.setattr('builtins.open', mock_open)
     mock_exists = MagicMock(return_value=False)
     monkeypatch.setattr("os.path.exists", mock_exists)
     with raises(Exception):
