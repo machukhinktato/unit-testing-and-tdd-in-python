90cc189898525b425b49327fe0ba43a74459e1eb
machukhinktato <tarabrinmv@gmail.com>
2021-01-19 19:12:19 +0300

    in progress

--- tst_doubles/doubles_overview.py
+++ tst_doubles/doubles_overview.py
@@ -1,3 +1,5 @@
+import pytest
+from pytest import raises
 from line_reader import readFromFile
 from unittest.mock import MagicMock
 
@@ -9,6 +11,19 @@ def test_returnsCorrectString(monkeypatch):
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr('builtins.open', mock_open)
+    mock_exists = MagicMock(return_value=True)
+    monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile('blah')
     mock_open.assert_called_once_with("blah", "r")
     assert result == "test line"
+
+
+def test_throwsExceptionWithBadFile(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr('builtins.open', mock_open)
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr("os.path.exists", mock_exists)
+    with raises(Exception):
+        result = readFromFile('blah')
--- tst_doubles/line_reader.py
+++ tst_doubles/line_reader.py
@@ -1,4 +1,9 @@
+import os
+
+
 def readFromFile(filename):
+    if not os.path.exists(filename):
+        raise Exception("Bad file")
     infile = open(filename, 'r')
     line = infile.readline()
     return line
